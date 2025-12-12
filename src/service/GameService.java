9642fe0b145bed8f098c4af8af92e21c3099ae4b
Luca Guido <l.guido@itsprodigi.it>
2025-12-12 13:14:45 +0100

    create method insertGame

--- src/service/GameService.java
+++ src/service/GameService.java
@@ -1,5 +1,15 @@
 package service;
 
-public class GameService {
+import java.util.ArrayList;
+import models.Game;
 
+
+public class GameService {
+	ArrayList<Game> games = new ArrayList<Game>();
+	
+	public void insertGame(String name, int realeseYear, int difficultLevel)
+	{	
+		Game game = new Game(name, realeseYear, difficultLevel);
+		this.games.add(game);
+	}
 }
