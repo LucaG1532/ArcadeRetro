f6e1c3690f727a1fd8003da32ba3d28b0c8b2692
Luca Guido <l.guido@itsprodigi.it>
2025-12-12 12:35:27 +0100

    Create Constructor and Setter and Getter Methods

--- src/models/Game.java
+++ src/models/Game.java
@@ -2,4 +2,39 @@ package models;
 
 public class Game {
 
+	private String name;
+	private int releaseYear; 
+	private int difficultLevel;
+	
+	public Game(String name, int releaseYear, int difficultLevel) {
+		this.name = name;
+		this.releaseYear = releaseYear;
+		this.difficultLevel = difficultLevel;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getReleaseYear() {
+		return releaseYear;
+	}
+
+	public void setReleaseYear(int releaseYear) {
+		this.releaseYear = releaseYear;
+	}
+
+	public int getDifficultLevel() {
+		return difficultLevel;
+	}
+
+	public void setDifficultLevel(int difficultLevel) {
+		this.difficultLevel = difficultLevel;
+	}
+	
+	
 }
