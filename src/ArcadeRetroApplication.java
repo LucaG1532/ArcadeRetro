cb0b313281d0fee8872f53be8014ad7986816470
Luca Guido <l.guido@itsprodigi.it>
2025-12-12 12:44:06 +0100

    create the while and the option

--- src/ArcadeRetroApplication.java
+++ src/ArcadeRetroApplication.java
@@ -1,8 +1,49 @@
+import java.util.Scanner;
 
 public class ArcadeRetroApplication {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		Scanner scanner = new Scanner(System.in);
+		
+		boolean isRunning = true;
+		
+		while(isRunning) 
+		{
+			System.out.println("ARCADE RETRO");
+			System.out.println("Scegli un'opzione tra le seguenti");
+			System.out.println("(1) Aggiungi gioco");
+			System.out.println("(2) Visualizza tutti i giochi");
+			System.out.println("(3) Cerca gioco");
+			System.out.println("(4) Filtro tra i giochi più difficili (Difficoltà >= 4");
+			System.out.println("(5) Esci");
+			String choose = scanner.nextLine();
+			switch(choose) 
+			{
+				case "1":
+				{
+					
+				}
+				case "2":
+				{
+					
+				}
+				case "3":
+				{
+					
+				}
+				case "4":
+				{
+					
+				}
+				case "5":
+				{
+					isRunning = false; 
+					break;
+				}
+			}
+			System.out.println("Opzione non valida");
+		}
 
 	}
 
