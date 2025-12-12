c79159666448e832870e945bcba89c4a458166ef
Luca Guido <l.guido@itsprodigi.it>
2025-12-12 13:45:29 +0100

    create method searchGame and printGame

--- src/service/GameService.java
+++ src/service/GameService.java
@@ -12,4 +12,25 @@ public class GameService {
 		Game game = new Game(name, realeseYear, difficultLevel);
 		this.games.add(game);
 	}
+	
+	public void printGameList() {
+		for (int i = 0; i < this.games.size(); i++) {
+			System.out.println( (i+1) + "° " + this.games.get(i).toString());
+		}
+	}
+	
+	public void searchGame(String search) 
+	{
+		  for(int i = 0; i < this.games.size(); i++) 
+		  {
+			  if(this.games.get(i).getName().equals(search))
+			  {
+				  System.out.println("Hai cercato il " + this.games.get(i).toString());
+			  }
+			  else 
+			  {
+				  System.out.println("Contatto non trovato");
+			  }
+		  }
+	}
 }
