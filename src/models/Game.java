c4fbae3228a4a4ba9cc450d69d53b70f0ea7ead1
Luca Guido <l.guido@itsprodigi.it>
2025-12-12 15:04:20 +0100

    cmment game.java

--- src/models/Game.java
+++ src/models/Game.java
@@ -1,17 +1,18 @@
 package models;
 
 public class Game {
-
+	// dichiarazioni variabili in modo privato
 	private String name;
 	private int releaseYear; 
 	private int difficultLevel;
 	
+	//creazione del costruttore
 	public Game(String name, int releaseYear, int difficultLevel) {
 		this.name = name;
 		this.releaseYear = releaseYear;
 		this.difficultLevel = difficultLevel;
 	}
-
+	//metodi getter and setter
 	public String getName() {
 		return name;
 	}
@@ -35,7 +36,8 @@ public class Game {
 	public void setDifficultLevel(int difficultLevel) {
 		this.difficultLevel = difficultLevel;
 	}
-
+	
+	// creazione metodo toStringh()
 	@Override
 	public String toString() {
 		return "Gioco --> nome: " + name + ", anno pubblicazione: " + releaseYear + ", difficoltà gioco: " + difficultLevel;
